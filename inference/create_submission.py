bdf1f96deebd467d155a48a0f58ac4fbdcb5a849
Doun Lee <67096173+Ldoun@users.noreply.github.com>
2025-06-18 20:30:26 +0900

    Update create_submission.py

--- inference/create_submission.py
+++ inference/create_submission.py
@@ -20,11 +20,25 @@ class TestDataset(Dataset):
         return len(self.df)
     
     def __getitem__(self, index):
-        image = Image.open(os.path.join('/home/20223185/data/HAI/', self.df.iloc[index]['img_path'][2:]))
+        image = Image.open(os.path.join('../HAI/', self.df.iloc[index]['img_path'][2:]))
         t_image = self.transform(image)
         return t_image
 
-model = SiglipForImageClassification.from_pretrained('result/0.00005_4_11/best_model', num_labels=393)
+config = LoraConfig(
+        r=16,
+        lora_alpha=16,
+        lora_dropout=0.1,
+        target_modules=["k_proj", "q_proj"],
+        bias="none",
+        modules_to_save=["classifier"],
+    )
+model = SiglipForImageClassification.from_pretrained('google/siglip2-giant-opt-patch16-384', num_labels=393)
+model = get_peft_model(model, config)
+
+state = torch.load('?_1/best_model.pt')
+model.load_state_dict(state)
+model.eval()
+model.cuda()
 
 transform_test = transforms.Compose([
         transforms.Resize((421, 421)),
@@ -33,12 +47,12 @@ transform_test = transforms.Compose([
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
 
-valid_dataset = ImageFolder(root='~/data/HAI/valid', transform=transform_test)
+valid_dataset = ImageFolder(root='../HAI/valid', transform=transform_test)
 valid_loader = DataLoader(
         valid_dataset, batch_size=32, shuffle=False, num_workers=4, #pin_memory=True
 )
 
-test_data = pd.read_csv('~/data/HAI/test.csv')
+test_data = pd.read_csv('HAI/test.csv')
 test_data.tail()
     
 dataset = TestDataset(test_data, transform_test)
@@ -59,9 +73,9 @@ idx_to_class = {v: k for k, v in valid_dataset.class_to_idx.items()}
 
 concated = np.concatenate(result)
 
-submission = pd.read_csv('~/data/HAI/sample_submission.csv')
+submission = pd.read_csv('../HAI/sample_submission.csv')
 
 for i in range(concated.shape[1]):
     submission[idx_to_class[i]] = concated[:, i]
 
-submission.to_csv('inference/0.00005_4_final.csv', index=False)
+submission.to_csv('0.00005_4_final.csv', index=False)
